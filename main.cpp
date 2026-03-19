b4e6ff64a16592e08e6db76e3321bcc62b9b1c92
Lorenzo <calderonlorenzo026@gmail.com>
2026-03-19 18:53:53 +0000

    added a45 code

--- main.cpp
+++ main.cpp
@@ -12,6 +12,38 @@ int main()
     cin >> num1 >> num2 >> num3;
 
     // TODO: Find min, max, and med among num1, num2, num3
+    if (num1 < num2 && num1 < num3) {
+        min = num1;
+        if (num2 < num3) {
+            med = num2;
+            max = num3;
+        } else {
+            med = num3;
+            max = num2;
+        }
+    }
+    
+    if (num2 < num1 && num2 < num3) {
+        min = num2;
+        if (num1 < num3) {
+            med = num1;
+            max = num3;
+        } else {
+            med = num3;
+            max = num1;
+        }
+    }
+
+    if (num3 < num1 && num3 < num2) {
+        min = num3;
+        if (num1 < num2) {
+            med = num1;
+            max = num2;
+        } else {
+            med = num2;
+            max = num1;
+        }
+    }
 
     cout << min << " " << med << " " << max << endl;
 }
